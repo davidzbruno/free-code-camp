c977ff813f4fd5e5d97750fcfc284e8d20e0cfff
James Renfrow <39011780+d3lta0ne@users.noreply.github.com>
2024-09-30 00:52:28 -0400

    feat: created script.sh to create all folders/files for challenges

---
+++ responsive-web-design/learn-css-colors-by-building-a-set-of-colored-markers/script.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+NUMBER_OF_DIRECTORIES=91
+
+read -p "How many directories should I create? " NUMBER_OF_DIRECTORIES
+
+function make_directory() {
+    local n=$1
+    for i in $(seq -w 1 $n)
+    do
+        if [ ! -d "step-$i" ]; then  # Directory DOES NOT EXIST
+            mkdir "step-$i"
+        fi
+        touch "step-$i/index.html"  # Create index.html for all challenges
+        if [ $i -ge 7 ]; then
+            touch "step-$i/styles.css"  # Create styles.css file for remaining challenges
+        fi
+    done
+}
+
+make_directory $NUMBER_OF_DIRECTORIES
