78b4c65a77aef26a2844beaf5b34999cbf3e30d0
James Renfrow <39011780+d3lta0ne@users.noreply.github.com>
2024-10-04 12:35:33 -0400

    solved: complex jsx object

---
+++ front-end-development-libraries/react/complex-jsx-element.jsx
@@ -0,0 +1,12 @@
+const JSX = (
+    <div>
+      <h1>Random Facts</h1>
+      <p>Here are some interesting tidbits:</p>
+      <ul>
+        <li>The honeybee is the only insect that produces food eaten by humans.</li>
+        <li>Sharks have been around longer than trees!</li>
+        <li>The Eiffel Tower can be 15 cm taller during the summer.</li>
+      </ul>
+    </div>
+  );
+  
